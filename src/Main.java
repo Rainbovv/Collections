31247a57edd59e9f09e3d0ad0185ed716b9f1ad9
Radu Ţurcanu <63318406+Rainbovv@users.noreply.github.com>
2020-06-16 01:21:14 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,33 +1,87 @@
-import java.util.ArrayList;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
 
-        ArrayList<String> devs = new ArrayList();
+        List<Double> temps = (ArrayList<Double>)getListOfRandomDoubles(31, -25.0, 25.0);
 
-        // adding data to the list
-        devs.add("Johny");
-        devs.add("Valerya");
-        devs.add("Tatyana");
-        devs.add("Peter");
-        devs.add("Marry");
 
-        // showing the initials
-        for (int i = 0; i < devs.size(); i++) {
-            System.out.printf("%d.   \"%c.\"%n",i + 1 , devs.get(i).charAt(0));
+        printList(temps);
+    }
+
+
+    static void printList(List<Double> list) {
+
+        System.out.println("\n###################################################");
+        System.out.print("       mo     tu     we     th     fi     sa     su");
+
+        for (int i = 0,  j = 1; i < list.size(); i++) {
+            if ( i == 0 || i % 7 == 0) {
+                System.out.printf("%n%d.", j++);
+            }
+            System.out.printf("%7.1f", list.get(i));
         }
 
-        // the longest name
-        int maxLength = 0;
+        System.out.println("\n###################################################\n");
+        System.out.printf("The min temperature:%10.1f%n" , minTemp(list));
+        System.out.printf("The max temperature:%10.1f%n" , maxTemp(list));
+        System.out.printf("The average temperature:%6.1f%n" , averageTemp(list));
+    }
 
-        for (String dev: devs) {
-            if (dev.length() > maxLength) maxLength = dev.length();
+
+    static List<Double> getListOfRandomDoubles(int size, double origin, double bound) {
+
+        List<Double> result = new ArrayList<>();
+        double[] randomDoubles = new Random().doubles(size, origin, bound).toArray();
+
+        for (int i = 0; i < size; i++) {
+            result.add(roundAvoid(randomDoubles[i],1));
         }
 
-        // swap values
-        String temp;
-        temp = devs.get(devs.size() - 1);
-        devs.set(devs.size() - 1, devs.get(0));
-        devs.set(0, temp);
+        return result;
+    }
+
+
+    static double roundAvoid(double value, int places) {
+
+        double scale = Math.pow(10, places);
+
+        return Math.round(value * scale) / scale;
+    }
+
+
+    static double minTemp(List<Double> list) {
+
+        double min = list.get(0);
+
+        for (double temp: list) {
+            if (temp < min) min = temp;
+        }
+
+        return min;
+    }
+
+
+    static double maxTemp(List<Double> list) {
+
+        double max = list.get(0);
+
+        for (double temp: list) {
+            if (temp > max) max = temp;
+        }
+
+        return max;
+    }
+
+
+    static double averageTemp(List<Double> list) {
+
+        double sum = 0;
+
+        for (double temp: list) {
+            sum += temp;
+        }
+
+        return roundAvoid(sum / list.size(),1);
     }
 }
